4f3229d845a18cd5ce3749bceb9c1cb4f5127e19
sharafan.o <sharafanoksana@gmail.com>
2025-04-21 18:25:48 +0300

    работа с тестированием

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,12 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { createHtmlPlugin } from "vite-plugin-html";
-import vueDevTools from "vite-plugin-vue-devtools";
-import pugPlugin from "vite-plugin-pug";
+// import { createHtmlPlugin } from "vite-plugin-html";
+// import vueDevTools from "vite-plugin-vue-devtools";
+// import pugPlugin from "vite-plugin-pug";
 
-const options = { pretty: true } // FIXME: pug pretty is deprecated!
-const locals = { name: "My Pug" }
+// const options = { pretty: true };
+// const locals = { name: "My Pug" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,11 +28,8 @@ export default defineConfig({
     host: true,
     port: 8080,
     proxy: {
-      // "/api": "http://localhost:3000/",
       "/api": {
         target: "http://localhost:3000/",
-        // target: "http://backend:3000/",
-        // target: "https://pizza.vue.htmlacademy.pro/",
         changeOrigin: true,
         rewrite : (path) => path.replace(/^\/api/, ""),
       },
